c5e3761166b64751aca66f3496fa132b849759c1
Maciej Piwowarczyk <mpiwo4@gmail.com>
2024-12-04 07:20:40 +0100

    day 1 part 1

--- DayOne.cs
+++ DayOne.cs
@@ -1,20 +1,52 @@
 ﻿namespace AdventOfCode;
 public class DayOne : Day
 {
-    private readonly string Input = Helper.GetInput(1).Result;
+    private readonly string Input = Helper.GetInput(1).Result.Trim();
     public override string PartOne()
     {
-        string output = string.Empty;
-        
+        int output = 0;
 
+        var splited = Input.Split("\n");
+        List<int> leftValues = [];
+        List<int> rightValues = [];
 
+        foreach (var line in splited)
+        {
+            var values = line.Split();
+            leftValues.Add(Convert.ToInt32(values[0]));
+            rightValues.Add(Convert.ToInt32(values[^1]));
+        }
 
+        leftValues.Sort();
+        rightValues.Sort();
 
-        return output;
+        for(int i = 0; i < leftValues.Count-1; i++)
+        {
+            output += Math.Abs(leftValues[i] - rightValues[i]);
+        }
+
+        return output.ToString();
     }
 
     public override string PartTwo()
     {
-        return base.PartTwo();
+        int output = 0;
+
+        var splited = Input.Split("\n");
+        List<int> leftValues = [];
+        List<int> rightValues = [];
+        
+        foreach (var lValue in leftValues)
+        {
+            int occurences = 0;
+            foreach(var rValue  in rightValues)
+            {
+                if (rValue == lValue) occurences++;
+            }
+            output += lValue * occurences;
+        }
+
+
+        return output.ToString();
     }
 }
