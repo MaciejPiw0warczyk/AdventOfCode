aec1a2fdba81d26a57cb6f6c2d55710170f78679
Maciej Piwowarczyk <mpiwo4@gmail.com>
2024-12-04 08:15:45 +0100

    day 2 part 2

---
+++ DayTwo.cs
@@ -0,0 +1,55 @@
+﻿namespace AdventOfCode;
+internal class DayTwo : Day
+{
+    private readonly string Input = Helper.GetInput(2).Result.Trim();
+    public override string PartOne()
+    {
+        int output = 0;
+        var splited = Input.Split("\n").Select(x => x.Split()).ToList();
+
+        foreach (var report in splited)
+        {
+            var levels = report.Select(x => Convert.ToInt32(x)).ToList(); //move conversion to splited init
+            bool isSafe = true;
+            bool incrementing = false;
+
+            if (levels[0] == levels[1])
+                continue;
+            else if (levels[0] < levels[1])
+                incrementing = true;
+            else
+                incrementing = false;
+
+            for (int i = 0; i < levels.Count - 1; i++)
+            {
+                if (i + 1 <= levels.Count - 1)
+                {
+                    if (levels[i] == levels[i + 1])
+                    { isSafe = false; break; }
+
+                    if (incrementing)
+                    {
+                        if (levels[i] > levels[i + 1])
+                        { isSafe = false; break; }
+                    }
+                    else if (levels[i] < levels[i + 1])
+                    { isSafe = false; break; }
+
+
+                    var diff = Math.Abs(levels[i] - levels[i + 1]);
+                    if (!(diff >= 1 && diff <= 3))
+                    { isSafe = false; break; }
+                }
+            }
+            if (isSafe)
+                output++;
+
+        }
+        return output.ToString();
+    }
+
+    public override string PartTwo()
+    {
+        return base.PartTwo();
+    }
+}
--- Program.cs
+++ Program.cs
@@ -7,8 +7,14 @@ internal class Program
         Helper.Year = args[1];
 
         var day = new DayOne();
-        
+        var day2 = new DayTwo();
+
+        Console.WriteLine("Day One");
         Console.WriteLine(day.PartOne());
         Console.WriteLine(day.PartTwo());
+
+        Console.WriteLine("Day Two");
+        Console.WriteLine(day2.PartOne());
+        Console.WriteLine(day2.PartTwo());
     }
 }
